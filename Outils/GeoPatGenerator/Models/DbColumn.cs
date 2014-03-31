347a066b5e3bb8b22c7b55c2ee908ff350427f4c
Loïc Lecuyer (Work station) <titi.tata@free.fr>
2014-03-31 12:02:34 +0200

    Projet de génaration

---
+++ Outils/GeoPatGenerator/Models/DbColumn.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emash.GeoPat.Generator.Models
+{
+    public  class DbColumn
+    {
+        public String DisplayName { get; set; }
+        public String Name { get; set; }
+        public String Id { get; set; }
+        public String DataType { get; set; }
+        public Boolean AllowNull { get; set; }
+        public Nullable<Int32> Length { get; set; }
+
+      
+        public String ToPostgreSqlDefinition()
+        {
+            string result = null;
+            if (this.DataType.StartsWith("VARCHAR"))
+            {
+                result = "character varying(" + Length.Value + ")";
+            }
+            else if (this.DataType.StartsWith("NUMBER"))
+            {
+                if (this.DataType.IndexOf(",") == -1)
+                { result = "integer"; }
+                else
+                { result = "real"; }
+               
+            }
+            else if (this.DataType.StartsWith("INTEGER"))
+            { result = "integer"; }
+            else if (this.DataType.StartsWith("SMALLINT"))
+            { result = "boolean"; }
+            else if (this.DataType.StartsWith("FLOAT"))
+            { result = "real"; }
+            else if (this.DataType.StartsWith("DATE"))
+            { result = "date"; }
+            else
+            {
+                Console.WriteLine(this.DataType);
+            }
+            if (!this.AllowNull && result != null)
+            { result += " NOT NULL"; }
+            return result;
+        }
+    }
+}
